276cf7675c8e50c2d3e0456bf3726b1efbdb8dbf
unknown <nmillar@NICKMILL-L3.EnerNOC.local>
2015-08-22 12:41:34 -0500

    Added homework functions.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    #store a list of a matrix and its inverse
+  xinv <- NULL
+  set <- function(y){ #sets the value of the matrix in the cache
+      x <<- y
+      xinv <<- NULL
+  }
+  get <- function() {x} #retrieves the value of the matrix
+  setinv <- function(inv) {xinv <<- inv} #sets the value of the matrix in the cache
+  getinv <- function() {xinv}
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -12,4 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+    xinv <- x$getinv()  
+    if(!is.null(xinv)){   #if inverse already cached, get from cache
+        message("Getting cached inverse.")
+        return(xinv)
+      }
+        a <- x$get() # retrieves matrix
+        xinv <- solve(a) #creates inverse of matrix
+        x$setinv(xinv) #sets the inverse to the cache
+        xinv #returns the inverse
+  }
